9f499c6ff111ae55bd0c16de998a25f624796d6d
Кучеров Роман <quoe@mail.ru>
2020-07-12 16:37:38 +0300

    Начало

---
+++ Assets/NPR Contour Drawing/ReadMe.txt
@@ -0,0 +1,9 @@
+Thank you for downloading ! :)
+
+Demo scene demonstrate all features, please refer it as example usage.
+Open it and click play, you will see the shader works on models.
+Hold left mouse button and move can rotate the main camera. Press W,S,A,D,Q,E to move.
+
+If you like it, please give us a good review on asset store. We will keep moving !
+Any question, suggestion or requesting, please contact qq_d_y@163.com.
+Hope we can help more and more unity3d developers.
---
+++ Assets/NPR Contour Drawing/Script/Demo.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Demo : MonoBehaviour
+{
+	public bool m_EnableContour = true;
+	public GameObject m_MainCamera;
+	private ContourDrawing[] m_Objs;
+	
+	void Start ()
+	{
+		QualitySettings.antiAliasing = 8;
+		m_Objs = GameObject.FindObjectsOfType<ContourDrawing> ();
+		for (int i = 0; i < m_Objs.Length; i++)
+			m_Objs[i].Initialize ();
+	}
+	void Update ()
+	{
+		for (int i = 0; i < m_Objs.Length; i++)
+		{
+			if (m_EnableContour)
+			{
+				m_Objs[i].FxEnable ();
+				m_Objs[i].UpdateSelfParameters ();
+				m_MainCamera.GetComponent<Skybox> ().enabled = true;
+			}
+			else
+			{
+				m_Objs[i].FxDisable ();
+				m_MainCamera.GetComponent<Skybox> ().enabled = false;
+			}
+		}
+	}
+	private void OnGUI()
+	{
+		int w = 250;
+		GUI.Box (new Rect (Screen.width / 2 - w / 2, 10, w, 25), "NPR Contour Drawing Demo");
+	}
+}
